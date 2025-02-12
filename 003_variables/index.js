69bc2a19bebc524e95862c475265bf9f1a3d7870
Satis <satis@Satiss-MacBook-Air.local>
2025-02-13 00:23:49 +0545

    Variables

---
+++ 003_variables/index.js
@@ -0,0 +1,56 @@
+
+let userName = "NextLevelCodes";
+
+let firstName="Satis";
+
+let lastName="Chaudhary";
+
+// var first_name= "First name"
+
+// const FirstNumber = 0
+
+
+
+// we can not defined variable like this 
+// let let = "let Variable"
+
+let fullName = firstName+ " " +lastName;
+// console.log(fullName)
+
+// console.log(x)
+
+{var x = 20};
+// console.log(x);
+
+// let age = 25;
+// let age =30;
+
+// var X = 0;
+
+{let y = 10};
+
+{const num1 = 20;}
+
+// console.log(num1);
+
+
+// console.log(y)
+
+
+
+
+
+const hoursInDay = 24;
+// console.log(hoursInDay)
+
+let IsGfBf = true;
+let isgfBf = false;
+
+
+
+let आयु = 21;
+let उमेर = 23;
+let ವಯಸ್ಸು = 25;
+// let आयु = 21;
+
+
