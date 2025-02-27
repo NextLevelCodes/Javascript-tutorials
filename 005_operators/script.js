5f35cbcc468d3bf2e0ac0b1629036e02942288f3
Satis <satis@Satiss-MacBook-Air.local>
2025-02-28 00:46:33 +0545

    operators

---
+++ 005_operators/script.js
@@ -0,0 +1,83 @@
+let x = 10;
+let y =2;
+// bodmas->bracket order divide multiplication addition subtraction
+let z = 10 - (3 + 10 *2) / 5;
+console.log(z);
+// 1.Arithmetic Operators
+// operand operator operand
+// let z = x+y;
+// console.log(z);
+// console.log("The sum of x and y is: ",x+y); //10+2=12
+// console.log("The sub of x and y is: ",x-y); //10-2=8
+// console.log("The mul of x and y is: ",x*y);//10*2=20
+// console.log("The div of x and y is: ",x/y); //10/2=5
+// console.log("The Modulus of x and y is: ",x%y); // 10%2=0
+// console.log("The Power of x and y is: ",x**y); //10**2=100
+
+// 2. Assignment Operators
+// console.log(x+=2) //x=x+2 = 12
+// console.log(x-=2) //x=x-2=8
+// console.log(x*=2) //x=x*2=20
+// console.log(x/=2) //x=x/2=5
+
+
+// 3. Logical Operators
+
+// && (logical and)
+// console.log(true && true);
+// console.log(false && true);
+// console.log(true && false);
+// console.log(false && false);
+// || (logical or)
+// console.log(true || true);
+// console.log(false || true);
+// console.log(true || false);
+// console.log(false || false);
+// ! (logical not)
+// console.log(!true,!1);
+// console.log(!false ,!0);
+
+// 4. Bitwise Operators
+// console.log(5 | 3)
+
+// x = -(x+1);
+// ~8=-(8+1)=-9
+// ~1=-(1+1)=-2
+// ~2=-(2+1)=-3
+// ~-2=-(-2+1)=1
+// ~-8=-(-8+1)=7
+
+// let a= null;
+// console.log(a??"Subscribe Now")
+
+
+// console.log(++x)
+// --x;
+// console.log(x)
+
+// let result = x + ++y;
+// console.log(result)
+
+// let result = x + --y;
+// console.log(result)
+// console.log(y)
+
+// let result = x + y++;
+// console.log(result)
+// console.log(y)
+
+// let result = x + y--;
+// console.log(result)
+// console.log(y)
+
+
+let firstName="Satis";
+let lastName = "Chaudhary"
+
+var fullName = firstName + " " +lastName;
+
+const yearOfBirth = 2000;
+
+let age = 25;
+let a;
+
