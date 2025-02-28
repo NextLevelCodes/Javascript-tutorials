00c10818d781e83e671799948f5668652d6dc742
Satis <satis@Satiss-MacBook-Air.local>
2025-02-28 23:21:02 +0545

    Dialog Boxes

---
+++ 006_dialog_boxes/script.js
@@ -0,0 +1,14 @@
+// alert("Hello world from alert")
+
+// let promptInput= prompt("Enter your age");
+
+// const isConfirmed=confirm("Are you sure?");
+
+// console.log("Hello world")
+
+const num1 = prompt("Enter number 1");
+const num2 = prompt("Enter number 2");
+
+const sum = Number(num1) + Number(num2);
+
+console.log("The sum is:",sum);
