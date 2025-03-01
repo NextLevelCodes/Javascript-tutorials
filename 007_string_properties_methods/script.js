d863b120b69f7da669b3f77f5f5e3364a588d71d
Satis <satis@Satiss-MacBook-Air.local>
2025-03-02 00:54:35 +0545

    String properties and Methods

---
+++ 007_string_properties_methods/script.js
@@ -0,0 +1,9 @@
+let firstName = "Satis 123123"
+let lastName = "Chaudahry";
+let fullName ="satis chaudhary";
+
+let initialsCapital = fullName.split(" ")[0][0] + fullName.split(" ")[1][0];
+
+let myIntro = `Hi, my name is ${firstName} ${lastName}`;
+
+let lats4Digit = '9898';
