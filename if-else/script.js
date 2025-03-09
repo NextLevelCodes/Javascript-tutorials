51820e167565f7f9747160f0e2c81a73b0925113
Satis <satis@Satiss-MacBook-Air.local>
2025-03-09 16:22:33 +0545

    If Else

---
+++ if-else/script.js
@@ -0,0 +1,32 @@
+// let a="coke";
+// if(a==="coke") {
+//     console.log("Bring coke")
+// }
+// else {
+//     console.log("Bring sprite")
+// }
+    
+// console.log("Program End")
+
+let age = Number(prompt("Enter your age:"));
+// let name = prompt("Enter your name:")
+// if(age >=18){
+//     console.log("You are eligible to vote");
+//     if(name ==="Satis"){
+//         console.log("Your name is : ",name)
+//     }else{
+//         console.log("Your name is : Unknown") 
+//     }
+// }else{
+//     console.log("You are not eligible to vote");
+// }
+
+if(age >=18 && age <=20){
+    console.log("You are eligible to vote");
+}
+else if(age >=18 && age <=30){
+    console.log("You are eligible to vote between 18 to 30");
+}
+else if(age >=18 && age <=35){
+    console.log("You are eligible to vote between 18 to 35");
+}
