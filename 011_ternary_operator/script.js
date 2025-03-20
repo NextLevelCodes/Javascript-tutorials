dba2a3a1e4ef57f1b2f740d096397b5773794bff
Satis <satis@Satiss-MacBook-Air.local>
2025-03-20 23:34:38 +0545

    ternary operator

---
+++ 011_ternary_operator/script.js
@@ -0,0 +1,15 @@
+
+// let userName = null ? "Satis" : "Unknown"
+
+// let gender = "F";
+
+// let role = `${gender.toLowerCase() === "f" ? "She" : "He"} is a Programmer`;
+
+
+// console.log(role)
+
+// let number = false ? "isNumber" : "isString" ? "Satis" :10;
+// console.log(number)
+
+let userName = null ? "Satis" : undefined ? "Female" :"Male";
+console.log(userName);
