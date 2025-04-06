9c575ebbc321146e6c0f15095d01e78132718680
Satis <satis@Satiss-MacBook-Air.local>
2025-04-06 12:27:43 +0545

    Objects

---
+++ 013_objects/script.js
@@ -0,0 +1,37 @@
+// let personName = "Satis";
+// let personAddress = "Ithari";
+
+// let userName = "name"
+// let user = {}
+// let user2 = {}
+
+let person = {
+    name:"Satis",
+    province:{
+        proName:"Koshi",
+        district:"Sunsari",
+        location:{
+            city:"Biratnagr"
+        }
+    } 
+}
+// let person2 = {
+//     name:"Satis",
+//     province:{
+//         proName:"Koshi",
+//         district:"Sunsari",
+//         location:{
+//             city:"Biratnagr"
+//         }
+//     } 
+// }
+console.log(person.province.location["city"])
+// person.name = "Updated Name";
+// person.age = 20;
+// person["age"] = 25;
+// console.log(person["full"+"Name"]);
+// console.log(person["name"])
+// console.log(person)
+// console.log(person[2+2])
+// console.log(person["name"])
+// console.log(person["address"])
