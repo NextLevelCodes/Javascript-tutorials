021b427ee6edfc2925604a3af602746d69058d74
Satis <satis@Satiss-MacBook-Air.local>
2025-04-20 13:30:40 +0545

    Array in Javscript

---
+++ 015_arrays/script.js
@@ -0,0 +1,22 @@
+// let a = [1,2,3,4,5,6];
+// console.log(a[1]);
+// console.log(a[6])
+
+// const colors = ["Red","Blue","Green","Black",null,undefined,true,10,{name:"Satis",b:{age:"20"}}]
+// colors[0] = "Yellow";
+// colors[3] = "White";
+
+// colors[5] = "indio"
+// console.log(colors.length)
+// console.log(colors)
+// console.log(colors[8].name)
+
+const colors = ["Red","Blue","Green","Black"]
+// colors[colors.length] = "White"
+// colors[colors.length] = "White"
+// colors[colors.length] = "White"
+// colors[colors.length] = "White"
+// colors.push("White")
+// console.log(colors)
+
+const a =[]
