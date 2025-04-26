457a813f9cedb9aa6f45b079d9b20b2739e9e4ed
Satis <satis@Satiss-MacBook-Air.local>
2025-04-26 09:05:27 +0545

    Array methods

---
+++ 016_array_methods/script.js
@@ -0,0 +1,15 @@
+const numbers = [9,8,7,6,5,4,3,2,1]
+const colors = ["Red","Blue","Green"]
+                // [0,1,2,3,4,5,6,7,8,9]
+// numbers.push(6,7,8);
+// numbers.pop()
+
+// numbers.unshift(12,2,3,4)
+// numbers.shift()
+
+// const numbers2 = [10,11,12]
+// console.log(numbers.concat())
+
+// console.log(numbers.slice(4))
+// console.log(numbers.slice(6,8))
+// console.log(numbers)
