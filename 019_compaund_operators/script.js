d8b3b8bd0a1b6d386331a60792a452406ecceda9
Satis <satis@Satiss-MacBook-Air.local>
2025-05-23 20:11:28 +0545

    Compaound and increment and decrement Operators

---
+++ 019_compaund_operators/script.js
@@ -0,0 +1,34 @@
+// debugger
+// let a = 10;
+
+// a+= 5 // a= a + 5
+// a+= 5 // a= a + 5
+// a+= 5 // a= a + 5
+// a+= 5 // a= a + 5
+// a+= 5 // a= a + 5
+// a+= 5 // a= a + 5
+// a= a + 5 // a= a + 5
+// a= a + 5 // a= a + 5
+// a= a + 5 // a= a + 5
+// a= a + 5 // a= a + 5
+// a= a + 5 // a= a + 5
+// console.log(a)
+
+// a= a - 1
+// a= a - 1
+// a= a - 1
+// a= a - 1
+
+
+// let a = 5;
+// let newA = ++a;
+
+// console.log(a++); // 5
+// console.log(a); // 6
+
+// let a = 10;
+// let newA = --a
+
+// const a = 10;
+// a++
+// a= a +1;
