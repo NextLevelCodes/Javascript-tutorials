45e239d95bb66bc6c6b60722b279e4949bcd4a7f
Satis <satis@Satiss-MacBook-Air.local>
2025-05-28 21:31:00 +0545

    Do While loop

---
+++ 021_do_while_loop/script.js
@@ -0,0 +1,9 @@
+
+console.log("Program Started")
+let i = 0;
+let fruits = ["apple","orange","grapes","Mango"]
+do {
+    console.log(fruits[i]);
+    i++;
+} while (i < fruits.length);
+console.log("Program Ended")
