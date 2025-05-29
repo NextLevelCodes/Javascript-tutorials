a0fbd2a15b4958920a70203451cc43821abd3670
Satis <satis@Satiss-MacBook-Air.local>
2025-05-29 16:56:46 +0545

    For Loop

---
+++ 022_for_loop/script.js
@@ -0,0 +1,15 @@
+
+console.log("Program Started")
+
+// for (initialization; condition; update) {
+//     // code block
+// }
+// debugger
+
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+
+console.log("Program Ended")
