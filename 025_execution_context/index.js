fcb5cb3f31ee2e5a4a6a7ff619b2ee82a4c5f64f
Satis <satis@Satiss-MacBook-Air.local>
2025-06-02 22:42:52 +0545

    Execution context

---
+++ 025_execution_context/index.js
@@ -0,0 +1,15 @@
+// debugger
+greet()
+var a = "Hello";
+let age = 25;
+
+function greet(){
+    const times = 10;
+    console.log(`${times} times Namste Sir!`);
+    start()
+}
+
+function start(){
+    let min = 5;
+    console.log(`statrs in ${min} min `)
+}
