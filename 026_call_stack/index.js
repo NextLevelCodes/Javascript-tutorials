e54ee43260e1729564c9ae68729c7bcb96d57bc6
Satis <satis@Satiss-MacBook-Air.local>
2025-06-04 18:14:13 +0545

    Call Stack

---
+++ 026_call_stack/index.js
@@ -0,0 +1,26 @@
+// debugger
+// greet()
+// var a = "Hello";
+// let age = 25;
+
+// function greet(){
+//     const times = 10;
+//     console.log(`${times} times Namaste Sir!`);
+//     start()
+// }
+
+// function lateStart(){
+//     console.log("Late start")
+// }
+
+// function start(){
+//     let min = 5;
+//     console.log(`statrs in ${min} min `)
+//     lateStart()
+// }
+
+function jpt(){
+    console.log("JPT");
+    jpt()
+}
+jpt()
