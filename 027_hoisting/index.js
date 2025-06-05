ea21d92cfecc3c176574afba43865015d805ade8
Satis <satis@Satiss-MacBook-Air.local>
2025-06-05 17:26:31 +0545

    Hoisting

---
+++ 027_hoisting/index.js
@@ -0,0 +1,26 @@
+// debugger
+
+var age = 25;
+// console.log(age)
+// debugger
+
+greet()
+
+function greet(){
+    console.log("Hello Sir");
+}
+
+
+// function declaration
+
+
+// debugger
+
+// // function expression 
+var greet = function(){
+    console.log("Hello sir")
+}
+
+// greet() 
+
+
