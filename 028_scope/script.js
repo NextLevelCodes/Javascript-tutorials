c55ba74f180008f5428a8f64721dccc26c085fa3
Satis <satis@Satiss-MacBook-Air.local>
2025-06-07 00:48:01 +0545

    Scope

---
+++ 028_scope/script.js
@@ -0,0 +1,25 @@
+
+let userName = "Satis";
+let userRole = "user";
+var age = 25;
+// debugger
+function sum(){
+    let userName = "NextLevelCodes"
+    let a = 10;
+    let b = 10;
+    console.log(a+b);
+    console.log(userName);
+}
+//  console.log(userName);
+
+const subtract = function(){
+    let x = 0;
+    const a = 20;
+    const b = 30;
+    console.log(b-a)
+    console.log(userName);
+ }
+// console.log(x);
+// sum();
+
+console.log("Program Ended")
