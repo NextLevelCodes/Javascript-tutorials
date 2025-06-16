5e2ae764e46930181ac294934b2db49a5f4e568e
Satis <satis@Satiss-MacBook-Air.local>
2025-06-16 16:50:19 +0545

    Event Loop

---
+++ 032_event_loop/index.js
@@ -0,0 +1,15 @@
+
+console.log("Program Start");
+
+for(let i = 1;i<=5;i++){
+    console.log(i)
+}
+
+// setTimeout(function(){
+//     console.log("Inside SetTimeout");
+// },3000);
+
+console.log("Program End");
+
+
+
