33f36e0eb3d070053b9134c191809fc71eeecd23
Satis <satis@Satiss-MacBook-Air.local>
2025-06-18 12:55:22 +0545

    Arrow Functions in javascript

---
+++ 034_arrow_functions/index.js
@@ -0,0 +1,29 @@
+
+// function declaration 
+// function greet(){
+//     console.log("Hello Sir");
+// }
+// greet();
+
+// function expression 
+// const greet = function (){
+//    console.log("Hello Sir");
+// }
+// greet();
+
+// Arrow function 
+// const greet = ()=>console.log("Hello Sir");
+
+// greet();
+
+const sum = (x,y)=>x+y;
+const square = (num)=> num*num;
+
+const greet = ()=>console.log("Hii");
+
+// setTimeout(()=>{
+//     console.log("Running settimeout after 3 sec")
+// },3000)
+
+
+sum(10,20);
