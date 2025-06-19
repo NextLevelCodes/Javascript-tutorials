822e6e18081ace8ce3b0e647ce9e72f1b28b1c38
Satis <satis@Satiss-MacBook-Air.local>
2025-06-19 19:33:25 +0545

    for of vs for in loop

---
+++ 035_for_of_vs_for_in/index.js
@@ -0,0 +1,34 @@
+
+const colors = ["Red","Blue","Green","Black"]
+
+// for(let i = 0; i < colors.length; i++){
+//     console.log(colors[i])
+// }
+
+// for(const clr of colors){
+//     console.log(clr);
+// }
+
+// for(const clr in colors){
+//     // debugger
+//     console.log(clr);
+// }
+
+const user = {
+    name:"Satis",
+    age:25,
+    role:"Frontend Developer",
+    address:"Ithari"
+}
+// const keys = Object.keys(user);
+// const values = Object.values(user);
+// for(let key of keys){
+//     console.log(key)
+// }
+// for(let value of values){
+//     console.log(value)
+// }
+// const result = Object.entries(user);
+// for(const key in user){
+//     console.log(`The key is ${key} and value is ${user[key]} `)
+// }
